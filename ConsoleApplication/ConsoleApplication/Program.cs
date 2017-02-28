efcc7098e791cde21554982a7599aca35e8f6a38
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-03-01 08:49:01 +1000

    Add odesk code
    Code delivered by odesk worker

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        [DllImport("Win32Random.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void CreateRng(int port);
+
+        [DllImport("Win32Random.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Fill(byte[] b, int size);
+
+        [DllImport("Win32Random.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DeleteRng();
+
+        static void Main(string[] args)
+        {
+            CreateRng(3);
+
+            Byte[] randombytes = new Byte[20];
+
+            int nbytes = Fill(randombytes, 20);
+
+            DeleteRng();
+        }
+    }
+}
